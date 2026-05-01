e8d0019b584a5b7046319db81f2e2d22dc8f6eed
Katherine Cobos <kcoboscaballero@gmail.com>
2026-05-01 00:29:52 -0300

    banco de preguntas de musica, cine y series

---
+++ src/data/preguntas.js
@@ -0,0 +1,236 @@
+export const PREGUNTAS = {
+
+  MUSICA: [
+    {
+      pregunta: "¿En qué año se publicó 'Thriller' de Michael Jackson?",
+      opciones: ["1980", "1982", "1984", "1986"],
+      correcta: 1,
+    },
+    {
+      pregunta: "¿Cómo se llama el álbum debut de Oasis?",
+      opciones: ["(What's the Story) Morning Glory?", "Definitely Maybe", "Be Here Now", "Heathen Chemistry"],
+      correcta: 1,
+    },
+    {
+      pregunta: "¿Quién es conocida como la 'Reina del Pop'?",
+      opciones: ["Beyoncé", "Rihanna", "Madonna", "Lady Gaga"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿De qué país es originaria la banda AC/DC?",
+      opciones: ["Reino Unido", "Estados Unidos", "Canadá", "Australia"],
+      correcta: 3,
+    },
+    {
+      pregunta: "¿Cuál es el álbum más vendido de la historia?",
+      opciones: ["Back in Black", "Thriller", "The Dark Side of the Moon", "Hotel California"],
+      correcta: 1,
+    },
+    {
+      pregunta: "¿Cuántos integrantes tiene originalmente BTS?",
+      opciones: ["5", "6", "7", "8"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿Qué banda argentina grabó 'La Balada del Diablo y la Muerte'?",
+      opciones: ["Patricio Rey", "Divididos", "Soda Stereo", "La Renga"],
+      correcta: 3,
+    },
+    {
+      pregunta: "¿En qué ciudad nacieron The Beatles?",
+      opciones: ["Londres", "Manchester", "Liverpool", "Birmingham"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿Cómo se llama el primer álbum de Taylor Swift?",
+      opciones: ["Fearless", "Speak Now", "Taylor Swift", "Red"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿Qué artista tiene más Grammys ganados en la historia?",
+      opciones: ["Michael Jackson", "Beyoncé", "Jay-Z", "Georg Solti"],
+      correcta: 3,
+    },
+    {
+      pregunta: "¿Cómo se llama la voz principal de Coldplay?",
+      opciones: ["Thom Yorke", "Chris Martin", "Bono", "Dave Grohl"],
+      correcta: 1,
+    },
+    {
+      pregunta: "¿Qué significa 'MTV'?",
+      opciones: ["Music Television", "Modern TV", "Music and TV", "Multimedia Television"],
+      correcta: 0,
+    },
+    {
+      pregunta: "¿Quién compuso 'Bohemian Rhapsody'?",
+      opciones: ["Brian May", "Roger Taylor", "Freddie Mercury", "John Deacon"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿En qué año se separó Soda Stereo?",
+      opciones: ["1995", "1997", "1999", "2001"],
+      correcta: 1,
+    },
+    {
+      pregunta: "¿Qué artista publicó el álbum 'Renaissance'?",
+      opciones: ["Rihanna", "Beyoncé", "SZA", "Lizzo"],
+      correcta: 1,
+    },
+  ],
+
+  CINE: [
+    {
+      pregunta: "¿Quién dirigió 'El Padrino' (1972)?",
+      opciones: ["Martin Scorsese", "Steven Spielberg", "Francis Ford Coppola", "Brian De Palma"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿Cuántas estatuillas ganó 'Titanic' en los Oscar de 1998?",
+      opciones: ["9", "11", "13", "14"],
+      correcta: 1,
+    },
+    {
+      pregunta: "¿Qué película tiene la frase 'Que la fuerza te acompañe'?",
+      opciones: ["Star Trek", "Dune", "Star Wars", "El Señor de los Anillos"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿Qué actor interpreta a Iron Man en el UCM?",
+      opciones: ["Chris Evans", "Chris Hemsworth", "Robert Downey Jr.", "Mark Ruffalo"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿En qué año se estrenó 'El Rey León' animado original?",
+      opciones: ["1992", "1993", "1994", "1995"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿Qué película argentina ganó el Oscar a Mejor Película Extranjera en 2010?",
+      opciones: ["Nueve Reinas", "El Secreto de sus Ojos", "Relatos Salvajes", "Zama"],
+      correcta: 1,
+    },
+    {
+      pregunta: "¿Quién interpretó a Jack en 'Titanic'?",
+      opciones: ["Brad Pitt", "Matt Damon", "Leonardo DiCaprio", "Johnny Depp"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿Qué película tiene la frase 'Houston, tenemos un problema'?",
+      opciones: ["Gravity", "Interstellar", "Apollo 13", "The Martian"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿De qué color es la pastilla que toma Neo en Matrix?",
+      opciones: ["Azul", "Roja", "Verde", "Blanca"],
+      correcta: 1,
+    },
+    {
+      pregunta: "¿Quién dirigió 'Avengers: Endgame'?",
+      opciones: ["Joss Whedon", "James Gunn", "Los hermanos Russo", "Jon Favreau"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿Qué estudio animó 'Toy Story' (1995)?",
+      opciones: ["DreamWorks", "Pixar", "Disney", "Blue Sky"],
+      correcta: 1,
+    },
+    {
+      pregunta: "¿Qué actor ganó el Oscar por 'El Renacido'?",
+      opciones: ["Tom Hardy", "Will Smith", "Leonardo DiCaprio", "Matt Damon"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿Cómo se llama el villano principal de 'El Señor de los Anillos'?",
+      opciones: ["Saruman", "Sauron", "Morgoth", "Nazgûl"],
+      correcta: 1,
+    },
+    {
+      pregunta: "¿En qué ciudad transcurre 'Relatos Salvajes'?",
+      opciones: ["Buenos Aires", "Córdoba", "Montevideo", "Santiago"],
+      correcta: 0,
+    },
+    {
+      pregunta: "¿Qué película de Christopher Nolan trata sobre sueños dentro de sueños?",
+      opciones: ["Memento", "Interstellar", "Tenet", "Inception"],
+      correcta: 3,
+    },
+  ],
+
+  SERIES: [
+    {
+      pregunta: "¿Cuántas temporadas tiene 'Breaking Bad'?",
+      opciones: ["4", "5", "6", "7"],
+      correcta: 1,
+    },
+    {
+      pregunta: "¿En qué ciudad transcurre 'Friends'?",
+      opciones: ["Los Ángeles", "Chicago", "Nueva York", "Boston"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿Quién es el creador de 'Game of Thrones'?",
+      opciones: ["J.J. Abrams", "David Benioff y D.B. Weiss", "Vince Gilligan", "Ryan Murphy"],
+      correcta: 1,
+    },
+    {
+      pregunta: "¿Cómo se llama el personaje principal de 'Stranger Things'?",
+      opciones: ["Will Byers", "Mike Wheeler", "Eleven", "Dustin Henderson"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿De qué trata 'El Juego del Calamar'?",
+      opciones: ["Supervivencia en el mar", "Juegos infantiles mortales por dinero", "Un torneo de videojuegos", "Una competencia de cocina"],
+      correcta: 1,
+    },
+    {
+      pregunta: "¿Qué serie tiene el personaje Walter White?",
+      opciones: ["Better Call Saul", "The Wire", "Breaking Bad", "Ozark"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿En qué plataforma se estrenó 'The Mandalorian'?",
+      opciones: ["Netflix", "HBO Max", "Disney+", "Amazon Prime"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿Cuántos episodios tiene la primera temporada de 'El Juego del Calamar'?",
+      opciones: ["7", "8", "9", "10"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿Cómo se llama la serie argentina sobre un hacker adolescente?",
+      opciones: ["Floricienta", "Casi Ángeles", "Clon", "Educando a Nina"],
+      correcta: 1,
+    },
+    {
+      pregunta: "¿Qué serie tiene el personaje de Tony Soprano?",
+      opciones: ["The Wire", "Boardwalk Empire", "The Sopranos", "Goodfellas"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿En qué ciudad transcurre 'Narcos'?",
+      opciones: ["Bogotá", "Medellín", "Cali", "Cartagena"],
+      correcta: 1,
+    },
+    {
+      pregunta: "¿Qué actriz protagoniza 'Bridgerton'?",
+      opciones: ["Phoebe Dynevor", "Simone Ashley", "Nicola Coughlan", "Ruby Stokes"],
+      correcta: 0,
+    },
+    {
+      pregunta: "¿De qué país es 'Dark', la serie de Netflix?",
+      opciones: ["Austria", "Suecia", "Alemania", "Suiza"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿Cuántas temporadas tiene 'The Office' (versión americana)?",
+      opciones: ["7", "8", "9", "10"],
+      correcta: 2,
+    },
+    {
+      pregunta: "¿Qué personaje dice 'Winter is coming' en Game of Thrones?",
+      opciones: ["Jon Snow", "Ned Stark", "Robb Stark", "Tyrion Lannister"],
+      correcta: 1,
+    },
+  ],
+};
